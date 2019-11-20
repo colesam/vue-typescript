c439c705525d9f00b2ecf8c49c3ea0e6314e491b
Samuel Cole <samuel@myglobaldata.com>
2019-11-20 13:19:44 -0600

    Add tailwind.css with example usage

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div class="App">
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
@@ -17,13 +17,12 @@ import HelloWorld from "./components/HelloWorld.vue";
 export default class App extends Vue {}
 </script>
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+/* Using tailwind inside style tags */
+.App {
+  @apply flex;
+  @apply flex-col;
+  @apply items-center;
+  @apply pt-10;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="hello">
+  <div class="bg-red-600">
     <h1>{{ msg }}</h1>
   </div>
 </template>
@@ -12,20 +12,3 @@ export default class HelloWorld extends Vue {
   @Prop() private msg!: string;
 }
 </script>
-
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
